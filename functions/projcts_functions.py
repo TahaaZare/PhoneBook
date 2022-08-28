644581180c396ff45e5fdff163d129a194e5db29
Taha Zare <tahaazre@gmail.com>
2022-08-28 18:03:00 +0430

    Fixed Code with def !

--- functions/projcts_functions.py
+++ functions/projcts_functions.py
@@ -1,6 +1,16 @@
 import time
 
 
+# region Fake Data
+
+def fake_data(contacts):
+    contacts['ali'] = '093333333333'
+    contacts['taha'] = '98317868767'
+    contacts['HA'] = '12312312312'
+
+
+# endregion
+
 # region menu
 
 def show_menu(item=None):
@@ -37,8 +47,7 @@ def add_contact(contacts):
                 print('Please Wait . . .')
                 time.sleep(2)
                 print('Success to add your contact :) ')
-
-    if mobile.isalpha():
+    elif mobile.isalpha():
         while mobile.isalpha():
             print(20 * '!')
             print('please just enter a valid number !!!')
@@ -55,5 +64,66 @@ def add_contact(contacts):
                         print('Please Wait . . .')
                         time.sleep(2)
                         print('Success to add your contact :) ')
+
+
+# endregion
+
+# region Search
+
+def search_name(contacts, name):
+    if name.lower() in contacts.keys() or name.upper() in contacts.keys():
+        print(f'"{name.lower()}"  his/her number is "{contacts.get(name)}"')
+    else:
+        time.sleep(1)
+        print(f'sorry theres no contact with this "{name}" name')
+
+
+# endregion
+
+# region Update
+
+def update_contact_name(contacts, name):
+    if name in contacts:
+        phoneNumber = contacts.get(name)
+        del contacts[name]
+        change_name = input('Please Enter a new name for your contact : ')
+        contacts[change_name] = phoneNumber
+        print(f'Success to change Contact name !')
+    else:
+        time.sleep(1)
+        print(18 * '/')
+        print(f'sorry theres no contact with this "{name}" name')
+
+
+def update_contact_number(contacts, contact_name_number):
+    if contact_name_number in contacts:
+        change_number = input('Please Enter a new number for your contact : ')
+        contacts[contact_name_number] = change_number
+        print(f'Success to change phone number !')
+
+
+# endregion
+
+# region Delete
+
+def delete(contacts, delete_contact_name):
+    if delete_contact_name in contacts:
+        delete_contact = input(f'Are u Sure to delete this {delete_contact_name} contact y/n ?')
+        if delete_contact.lower() == 'y' or delete_contact.upper() == 'Y':
+            del contacts[delete_contact_name]
+            print(f'deleting {delete_contact_name} Contact ! Please Wait . . .')
+            time.sleep(2)
+            print('The deletion was successful !')
+        elif delete_contact.lower() == 'n' or delete_contact.upper() == 'N':
+            print('Ok i Keep this contact for Y !')
+            time.sleep(1)
+            show_menu()
+        else:
+            print('Invalid Input !')
+            print('Please tyr Later !')
+            time.sleep(2)
+            show_menu()
+    else:
+        print(f'Theres no contact with this {delete_contact_name} name !')
 
 # endregion
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from functions.projcts_functions import *
 import time
 
-contacts = {'taha': '09333130432'}
-
+contacts = {}
+fake_data(contacts)
 # region Show Menu
 
 show_menu()
@@ -27,39 +27,21 @@ while True:
     elif item == '3':
         # region Search Contact
         name = input('please enter name for search : ')
-        if name.lower() in contacts.keys() or name.upper() in contacts.keys():
-            print(f'{name.lower()}  his/her number is ?')
-        else:
-            time.sleep(1)
-            print(f'sorry theres no contact with this "{name}" name')
+        search_name(contacts, name)
         # endregion
     elif item == '4':
         # region Update Contact
         update_item = input('1-Update Name 2-Update Number ? ')
         if update_item == '1':
             # region Change Contact Name
-
             contact_name = input('Please enter a contact name for update : ')
-            if contact_name in contacts:
-                phoneNumber = contacts.get(contact_name)
-                del contacts[contact_name]
-                change_name = input('Please Enter a new name for your contact : ')
-                contacts[change_name] = phoneNumber
-                print(f'Success to change Contact name !')
-            else:
-                time.sleep(1)
-                print(18 * '/')
-                print(f'sorry theres no contact with this "{contact_name}" name')
+            update_contact_name(contacts, contact_name)
             print(28 * '/')
-
             # endregion
         elif update_item == '2':
             # region Change Phone Number
             contact_name_number = input('Please enter a contact name for update : ')
-            if contact_name_number in contacts:
-                change_number = input('Please Enter a new number for your contact : ')
-                contacts[contact_name_number] = change_number
-                print(f'Success to change phone number !')
+            update_contact_number(contacts, contact_name_number)
             print(28 * '/')
 
             # endregion
@@ -71,26 +53,14 @@ while True:
                 update_item = input('1-Update Name 2-Update Number ? ')
                 if update_item == '1':
                     # region Change Contact Name
-
                     contact_name = input('Please enter a contact name for update : ')
-                    if contact_name in contacts:
-                        change_name = input('Please Enter a new name for your contact : ')
-                        contacts[contact_name] = change_name
-                        print(f'Success to change Contact name !')
-                    else:
-                        time.sleep(1)
-                        print(18 * '/')
-                        print(f'sorry theres no contact with this "{contact_name}" name')
+                    update_contact_name(contacts, contact_name)
                     print(28 * '/')
-
                     # endregion
                 elif update_item == '2':
                     # region Change Phone Number
                     contact_name_number = input('Please enter a contact name for update : ')
-                    if contact_name_number in contacts:
-                        change_number = input('Please Enter a new number for your contact : ')
-                        contacts[contact_name_number] = change_number
-                        print(f'Success to change phone number !')
+                    update_contact_number(contacts, contact_name_number)
                     print(28 * '/')
 
                     # endregion
@@ -98,25 +68,7 @@ while True:
     elif item == '5':
         # region Delete Contact
         delete_contact_name = input('Please Enter a contact name for delete : ')
-        if delete_contact_name in contacts:
-            delete_contact = input(f'Are u Sure to delete this {delete_contact_name} contact y/n ?')
-            if delete_contact.lower() == 'y' or delete_contact.upper() == 'Y':
-                del contacts[delete_contact_name]
-                print(f'deleting {delete_contact_name} Contact ! Please Wait . . .')
-                time.sleep(2)
-                print('The deletion was successful !')
-            elif delete_contact.lower() == 'n' or delete_contact.upper() == 'N':
-                print('Ok i Keep this contact for Y !')
-                time.sleep(1)
-                show_menu()
-            else:
-                print('Invalid Input !')
-                print('Please tyr Later !')
-                time.sleep(2)
-                show_menu()
-        else:
-            print(f'Theres no contact with this {delete_contact_name} name !')
-            # if delete_contact_name
+        delete(contacts, delete_contact_name)
         # endregion
     elif item == '0':
         # region Exit
@@ -132,6 +84,8 @@ while True:
         # endregion
     elif item.lower() == 'help' or item.upper() == 'HELP':
         show_menu()
+    elif item.lower() == 'exit' or item.upper() == 'EXIT':
+        exit()
     else:
         print(20 * '/')
         print('Invalid Item !!!!')
